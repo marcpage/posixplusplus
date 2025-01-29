20fbabee3cb3cee75f37da69338f5f40e20454c0
Marc Page <MarcAllenPage@gmail.com>
2025-01-28 20:09:56 -0600

    Adding all sorts of time functionality

---
+++ src/inc/PsxTime.h
@@ -0,0 +1,348 @@
+#pragma once
+
+#include "PsxException.h"
+#include "PsxErrno.h"
+#include <math.h>
+#include <string>
+#include <sys/time.h>
+#include <time.h>
+
+namespace psx {
+
+class Time {
+public:
+    enum Span { Seconds, Minutes, Hours, Days, Weeks };
+    enum Month {
+        Jan,
+        January = Jan,
+        Feb,
+        February = Feb,
+        Mar,
+        March = Mar,
+        Apr,
+        April = Apr,
+        May,
+        Jun,
+        June = Jun,
+        Jul,
+        July = Jul,
+        Aug,
+        August = Aug,
+        Sep,
+        September = Sep,
+        Oct,
+        October = Oct,
+        Nov,
+        November = Nov,
+        Dec,
+        December = Dec,
+    };
+    enum CivilianHour { AM, PM };
+    enum Location { GMT, Local };
+    enum Epoch {UNIX, DOS, Mac, Cocoa};
+    typedef std::string String;
+
+    static Time now();
+    static Time epoch(Epoch epc=UNIX);
+
+    Time(clockid_t clock=CLOCK_REALTIME);
+    explicit Time(const time_t &time);
+    explicit Time(const timeval &time);
+    explicit Time(const timespec &time);
+    explicit Time(tm &time, Location location=Local);
+    explicit Time(const double &time);
+    Time(int year, Month month, int day, int hour24=0, int minutes=0, double seconds=0.0, Location location=Local);
+    Time(int year, Month month, int day, int hour, CivilianHour ampm, int minutes=0, double seconds=0.0, Location location=Local);
+
+    operator time_t() const;
+    operator timeval() const;
+    Time &operator+=(double secs);
+    Time operator+(double secs) const;
+    Time &operator-=(double secs);
+    Time operator-(double secs) const;
+    double operator-(const Time &other) const;
+    bool operator==(const Time &other) const;
+    bool operator!=(const Time &other) const;
+    bool operator<(const Time &other) const;
+    bool operator>(const Time &other) const;
+    bool operator<=(const Time &other) const;
+    bool operator>=(const Time &other) const;
+
+    int compare(const Time &other) const;
+    double seconds_since(const Time &epoch) const;
+    Time &add(double value, Span span=Seconds);
+    tm &gmt(tm &time) const;
+    tm &local(tm &time) const;
+    tm &location(tm &time, Location loc) const;
+    timeval &value(timeval &tv) const;
+    timespec &value(timespec &ts) const;
+    String &format(const String &fmt, String &buffer, Location location=Local) const;
+    String format(const String &fmt, Location location=Local) const;
+
+private:
+    timespec _time;
+
+    static timespec _init(tm &time, double fractionalSeconds, Location location);
+    static timespec _init(int year, Month month, int day, int hour24, int minutes, double seconds, Location location);
+    static int _ampm_to_hour24(int hour, CivilianHour ampm);
+    template<typename S>
+    static void clear(S &s);
+};
+
+template<typename S>
+inline void Time::clear(S &s) {
+    ::memset(&s, 0, sizeof(s));
+}
+
+inline Time Time::now() {
+    return Time();
+}
+
+inline Time Time::epoch(Epoch epc) {
+    int year = 0;
+
+    switch(epc) {
+        case UNIX: // Jan 1 1970 00:00:00 UTC
+            return Time(static_cast<time_t>(0)); // faster
+        case DOS:
+            year = 1980;
+            break;
+        case Mac:
+            year = 1904;
+            break;
+        case Cocoa:
+            year = 2001;
+            break;
+        default:
+            PsxThrow("Unkown Epoch"); // NOTEST
+    }
+    return Time(year, January, 1, GMT);
+}
+
+inline Time::Time(clockid_t clock)
+    :_time{0,0} {
+    ErrnoOnNegative(::clock_gettime(clock, &_time));
+}
+
+inline Time::Time(const time_t &time)
+    :_time{time,0} {}
+
+inline Time::Time(const timeval &time)
+    :_time{time.tv_sec, time.tv_usec} {
+    constexpr auto kNanoSecondsPerMicrosecond = 1000;
+    _time.tv_nsec *= kNanoSecondsPerMicrosecond;
+}
+
+inline Time::Time(const timespec &time) 
+    :_time(time) {}
+
+inline Time::Time(tm &time, Location location)
+    :_time(_init(time, 0.0, location)) {}
+
+inline Time::Time(const double &time)
+    :_time{0,0} {
+    constexpr auto kNanosecondsPerSecond = 1000000000.0;
+    const double wholeSeconds = ::floor(time);
+
+    _time.tv_sec = static_cast<time_t>(wholeSeconds);
+    _time.tv_nsec = static_cast<long>( (time - wholeSeconds) * kNanosecondsPerSecond );
+}
+
+inline Time::Time(int year, Month month, int day, int hour24, int minutes, double seconds, Location location)
+    :_time(_init(year, month, day, hour24, minutes, seconds, location)) {}
+
+inline Time::Time(int year, Month month, int day, int hour, CivilianHour ampm, int minutes, double seconds, Location location)
+    :_time(_init(year, month, day, _ampm_to_hour24(hour, ampm), minutes, seconds, location)) {}
+
+inline Time::operator time_t() const {
+    return _time.tv_sec;
+}
+
+inline Time::operator timeval() const {
+    struct timeval timeValue;
+    clear(timeValue);
+    return value(timeValue);
+}
+
+inline Time &Time::operator+=(double secs) {
+    return add(secs, Seconds);
+}
+
+inline Time Time::operator+(double secs) const {
+    return Time(*this).add(secs, Seconds);
+}
+
+inline Time &Time::operator-=(double secs) {
+    return add(-1 * secs, Seconds);
+}
+
+inline Time Time::operator-(double secs) const {
+    return Time(*this).add(-1 * secs, Seconds);
+}
+
+inline double Time::operator-(const Time &other) const {
+    return seconds_since(epoch()) - other.seconds_since(epoch());
+}
+
+inline bool Time::operator==(const Time &other) const {
+    return compare(other) == 0;
+}
+
+inline bool Time::operator!=(const Time &other) const {
+    return compare(other) != 0;
+}
+
+inline bool Time::operator<(const Time &other) const {
+    return compare(other) < 0;
+}
+
+inline bool Time::operator>(const Time &other) const {
+    return compare(other) > 0;
+}
+
+inline bool Time::operator<=(const Time &other) const {
+    return compare(other) <= 0;
+}
+
+inline bool Time::operator>=(const Time &other) const {
+    return compare(other) >= 0;
+}
+
+inline int Time::compare(const Time &other) const {
+    if (_time.tv_sec < other._time.tv_sec) {
+        return -1;
+    }
+
+    if (_time.tv_sec > other._time.tv_sec) {
+        return 1;
+    }
+
+    if (_time.tv_nsec < other._time.tv_nsec) {
+        return -1;
+    }
+
+    if (_time.tv_nsec > other._time.tv_nsec) {
+        return 1;
+    }
+
+    return 0;
+}
+
+inline double Time::seconds_since(const Time &epoch) const {
+    constexpr auto kNanosecondsPerSecond = 1000000000.0;
+    return double(_time.tv_sec - epoch._time.tv_sec)
+            + ( double(_time.tv_nsec - epoch._time.tv_nsec) 
+                / kNanosecondsPerSecond );
+}
+
+inline Time &Time::add(double value, Span span) {
+    switch(span) {
+        case Weeks:
+            value *= 7.0;
+        case Days:
+            value *= 24.0;
+        case Hours:
+            value *= 60.0;
+        case Minutes:
+            value *= 60.0;
+        case Seconds:
+            break;
+        default:
+            PsxThrow("Unknown timespan"); // NOTEST
+    }
+    return *this = Time(seconds_since(epoch()) + value);
+}
+
+inline tm &Time::gmt(tm &time) const {
+    clear(time);
+    const time_t value = _time.tv_sec;
+    PsxThrowIfNull(::gmtime_r(&value, &time));
+    return time;
+}
+
+inline tm &Time::local(tm &time) const {
+    clear(time);
+    const time_t value = _time.tv_sec;
+    PsxThrowIfNull(::localtime_r(&value, &time));
+    return time;
+}
+
+inline tm &Time::location(tm &time, Location loc) const {
+    switch(loc) {
+        case GMT:
+            return gmt(time); // time cleared in here
+        case Local:
+            return local(time); // time cleared in here
+        default:
+            PsxThrow("Unknown location"); // NOTEST
+    }
+}
+
+inline timeval &Time::value(timeval &tv) const {
+    constexpr auto kNanoSecondsPerMicrosecond = 1000;
+    tv.tv_sec = _time.tv_sec;
+    tv.tv_usec = static_cast<suseconds_t>(_time.tv_nsec / kNanoSecondsPerMicrosecond);
+    return tv;
+}
+
+inline timespec &Time::value(timespec &ts) const {
+    return ts = _time;
+}
+
+inline Time::String &Time::format(const String &fmt, String &buffer, Location loc) const {
+    size_t size;
+    struct tm timeValue;
+
+    location(timeValue, loc); // clears timeValue
+    buffer.assign(fmt.length() * 15, '\0');
+    size = ::strftime(const_cast<char *>(buffer.data()), buffer.length(), fmt.c_str(), &timeValue);
+    buffer.erase(size);
+    PsxAssert(size != 0 || fmt.size() == 0 || fmt == "%p");
+    return buffer;
+}
+
+inline Time::String Time::format(const String &fmt, Location location) const {
+    String buffer;
+
+    return format(fmt, buffer, location);
+}
+
+inline timespec Time::_init(tm &time, double fractionalSeconds, Location location) {
+    timespec spec;
+    clear(spec);
+    constexpr auto kNanosecondsPerSecond = 1000000000.0;
+    PsxAssert(fractionalSeconds < 1.0);
+    PsxAssert(fractionalSeconds >= 0.0);
+    PsxAssert( (location == GMT) || (location == Local));
+    spec.tv_sec = location == GMT ? ::timegm(&time) : ::mktime(&time);
+    PsxAssert(spec.tv_sec != static_cast<time_t>(-1));
+    spec.tv_nsec = static_cast<long>(fractionalSeconds * kNanosecondsPerSecond);
+    return spec;
+}
+
+inline timespec Time::_init(int year, Month month, int day, int hour24, int minutes, double seconds, Location location) {
+    struct tm date;
+    clear(date);
+    const double wholeSeconds = ::floor(seconds);
+
+    date.tm_year = year - 1900;
+    date.tm_mon = static_cast<int>(month);
+    date.tm_mday = day;
+    date.tm_hour = hour24;
+    date.tm_min = minutes;
+    date.tm_sec = static_cast<int>(wholeSeconds);
+    return _init(date, seconds - wholeSeconds, location);
+}
+
+inline int Time::_ampm_to_hour24(int hour, CivilianHour ampm) {
+    switch(ampm) {
+        case AM:
+            return hour == 12 ? 0 : hour;
+        case PM:
+            return hour + 12;
+        default:
+            PsxThrow("Unknown CivilianHour"); // NOTEST
+    }
+}
+
+} // namespace psx
---
+++ src/tests/test_PsxTime.cpp
@@ -0,0 +1,125 @@
+#include "../inc/PsxTime.h"
+#include "test.h"
+
+START_TESTS
+
+    auto t1 = psx::Time(2018, psx::Time::Feb, 5, 14, 30, 15);
+    psx::Time copy1(t1);
+
+    ASSERT( t1 == copy1 );
+    ASSERT(t1.format("%A") == "Monday");
+    ASSERT(t1.format("%a") == "Mon");
+    ASSERT(t1.format("%B") == "February");
+    ASSERT(t1.format("%b") == "Feb");
+    ASSERT(t1.format("%C") == "20");
+    ASSERT(t1.format("%D") == "02/05/18");
+    ASSERT(t1.format("%d") == "05");
+    ASSERT(t1.format("%F") == "2018-02-05");
+    ASSERT(t1.format("%M") == "30");
+    ASSERT(t1.format("%m") == "02");
+    ASSERT(t1.format("%p") == "PM");
+    ASSERT(t1.format("%S") == "15");
+
+    ASSERT(t1.format("%A", psx::Time::GMT) == "Monday");
+    ASSERT(t1.format("%a", psx::Time::GMT) == "Mon");
+    ASSERT(t1.format("%B", psx::Time::GMT) == "February");
+    ASSERT(t1.format("%b", psx::Time::GMT) == "Feb");
+    ASSERT(t1.format("%C", psx::Time::GMT) == "20");
+    ASSERT(t1.format("%D", psx::Time::GMT) == "02/05/18");
+    ASSERT(t1.format("%d", psx::Time::GMT) == "05");
+    ASSERT(t1.format("%F", psx::Time::GMT) == "2018-02-05");
+    ASSERT(t1.format("%M", psx::Time::GMT) == "30");
+    ASSERT(t1.format("%m", psx::Time::GMT) == "02");
+    ASSERT(t1.format("%p", psx::Time::GMT) == "PM");
+    ASSERT(t1.format("%S", psx::Time::GMT) == "15");
+
+    time_t now;
+    time(&now);
+    ASSERT(static_cast<time_t>(psx::Time::now()) == now);
+    time(&now);
+    ASSERT(psx::Time::now() - psx::Time(now) < 1.0);
+
+    struct timeval timeValue;
+    ErrnoOnNegative(::gettimeofday(&timeValue, NULL));
+    ASSERT(static_cast<timeval>(psx::Time::now()).tv_sec == timeValue.tv_sec);
+    ErrnoOnNegative(::gettimeofday(&timeValue, NULL));
+    ASSERT(psx::Time::now() - psx::Time(timeValue) < 1.0);
+
+    timespec timespecValue, timespecValue2;
+    ErrnoOnNegative(::clock_gettime(CLOCK_REALTIME, &timespecValue));
+    ASSERT(psx::Time::now() - psx::Time(timespecValue) < 1.0);
+    ASSERT(psx::Time(timespecValue).value(timespecValue2).tv_nsec == timespecValue.tv_nsec);
+
+    struct tm date;
+    date.tm_year = 120;
+    date.tm_mon = 5;
+    date.tm_mday = 28;
+    date.tm_hour = 20;
+    date.tm_min = 57;
+    date.tm_sec = 54;
+    //const char *dateFormat = "%Y/%m/%d %H:%M:%S";
+
+    ASSERT(psx::Time(date, psx::Time::GMT) 
+            == psx::Time(2020, psx::Time::June, 28, 20, 57, 54, psx::Time::GMT));
+    ASSERT(psx::Time(date, psx::Time::GMT)
+            == psx::Time(2020, psx::Time::June, 28, 8, psx::Time::PM, 57, 54, psx::Time::GMT));
+
+    ASSERT(psx::Time() - ((psx::Time() + 5.0) - 5.0) < 1.0);
+    ASSERT(psx::Time() - ((psx::Time() + 5.0) - 5.0) > -1.0);
+
+    ASSERT(psx::Time() - (psx::Time() -= 5.0) > 4.9);
+    ASSERT((psx::Time() += 5.0) - psx::Time() > 4.9);
+
+    ASSERT(psx::Time(2020, psx::Time::June, 28, 20, 57, 54, psx::Time::GMT) 
+            == psx::Time(2020, psx::Time::June, 28, 8, psx::Time::PM, 57, 54, psx::Time::GMT));
+
+    ASSERT(psx::Time(2020, psx::Time::June, 28, 8, 57, 54, psx::Time::GMT) 
+            == psx::Time(2020, psx::Time::June, 28, 8, psx::Time::AM, 57, 54, psx::Time::GMT));
+
+    ASSERT((psx::Time() + 0.001) != psx::Time());
+    ASSERT((psx::Time() += 1.0) > psx::Time());
+    ASSERT((psx::Time() -= 1.0) < psx::Time());
+    ASSERT((psx::Time() += 1.0) >= psx::Time());
+    ASSERT((psx::Time() -= 1.0) <= psx::Time());
+
+    ASSERT((psx::Time() += -0.1) <= psx::Time());
+    ASSERT((psx::Time() -= 0.1) <= psx::Time());
+
+    ASSERT((psx::Time() += 0.1) >= psx::Time());
+    ASSERT((psx::Time() -= -0.1) >= psx::Time());
+
+    ASSERT(fabs(psx::Time().add(1.0, psx::Time::Seconds) 
+                - (psx::Time() + 1.0)) 
+                < 1.0);
+    ASSERT(fabs(psx::Time().add(1.0, psx::Time::Minutes) 
+                - (psx::Time() + 60.0)) 
+                < 1.0);
+    ASSERT(fabs(psx::Time().add(1.0, psx::Time::Hours) 
+                - (psx::Time() + 3600.0)) 
+                < 1.0);
+    ASSERT(fabs(psx::Time().add(1.0, psx::Time::Days) 
+                - (psx::Time() + 24.0 * 3600.0)) 
+                < 1.0);
+    ASSERT(fabs(psx::Time().add(1.0, psx::Time::Weeks) 
+                - (psx::Time() + 7.0 * 24.0 * 3600.0)) 
+                < 1.0);
+
+    ASSERT(psx::Time::epoch(psx::Time::DOS) 
+            == psx::Time(1980, psx::Time::Jan, 1, psx::Time::GMT));
+    ASSERT(psx::Time::epoch(psx::Time::Mac) 
+            == psx::Time(1904, psx::Time::Jan, 1, psx::Time::GMT));
+    ASSERT(psx::Time::epoch(psx::Time::Cocoa) 
+            == psx::Time(2001, psx::Time::Jan, 1, psx::Time::GMT));
+
+    psx::Time t2, t3, t4;
+
+    ASSERT(t1 < t2);
+    ASSERT(t2 <= t3);
+    ASSERT(t3 <= t4);
+
+    ASSERT(t2 > t1);
+    ASSERT(t3 >= t2);
+    ASSERT(t4 >= t3);
+    
+END_TESTS
+
