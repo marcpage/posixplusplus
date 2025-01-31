e97eb2f1f666ddfdd9c57fed11329b9b85b2b9f3
Marc Page <MarcAllenPage@gmail.com>
2025-01-31 15:33:31 -0600

    fix assertion

--- src/tests/coverage.cpp
+++ src/tests/coverage.cpp
@@ -81,6 +81,10 @@ static void parse_gcov(const string &path) {
         }
         
         const auto firstField = line.find(":"); // status
+        if (firstField != string::npos) {
+            printf("WHAT: %s\n", line.c_str());
+            continue
+        }
         PsxAssert(firstField != string::npos);
         const auto secondField = line.find(":", firstField + 1); // line number
         PsxAssert(secondField != string::npos);
