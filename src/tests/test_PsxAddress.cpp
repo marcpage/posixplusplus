cab34d1319ebccf3882b22549a9e5802a9f79db7
Marc Page <MarcAllenPage@gmail.com>
2025-02-01 16:27:36 -0600

    debug

--- src/tests/test_PsxAddress.cpp
+++ src/tests/test_PsxAddress.cpp
@@ -8,6 +8,8 @@ START_TESTS
     auto apple = IPv4("apple.com", 80);
     auto copy = IPv4(apple.get(), apple.size());
 
+    printf("apple.name() == '%s'\n", apple.name().c_str());
+    printf("copy.name() == '%s'\n", copy.name().c_str());
     ASSERT(apple.name() == copy.name());
     ASSERT(IPv4().name() == string("0.0.0.0"));
     ASSERT(IPv6().name() == string("::"));
