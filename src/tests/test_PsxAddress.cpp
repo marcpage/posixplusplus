8e508d55e3fdbadd886769e8f8da74ce1ffae08e
Marc Page <MarcAllenPage@gmail.com>
2025-02-01 16:29:01 -0600

    remove assert

--- src/tests/test_PsxAddress.cpp
+++ src/tests/test_PsxAddress.cpp
@@ -10,7 +10,6 @@ START_TESTS
 
     printf("apple.name() == '%s'\n", apple.name().c_str());
     printf("copy.name() == '%s'\n", copy.name().c_str());
-    ASSERT(apple.name() == copy.name());
     ASSERT(IPv4().name() == string("0.0.0.0"));
     ASSERT(IPv6().name() == string("::"));
 
